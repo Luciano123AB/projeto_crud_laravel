a6861b56e28ef18761e4c0aa40c0d9bba8b6def6
Luciano123AB <Lucianoedustefa24032004@gmail.com>
2025-07-01 19:59:48 -0300

    001

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/", function () {
+    echo "Hello World!";
+});
+
+Route::get("/about", function(){
+    echo "About us";
 });
