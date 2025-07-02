c5b77a52e55987530367b36f79710d149a6cf50b
Luciano123AB <Lucianoedustefa24032004@gmail.com>
2025-07-01 21:13:10 -0300

    004(Receber_Parametros_Routes)

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,9 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-<body>
-    
+<body>    
     <h1>Welcome View and Blade!</h1>
+    <hr>
 
+    <h3>The value is: <?= $value ?></h3>
 </body>
 </html>
