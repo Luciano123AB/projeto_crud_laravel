e7dec8fa2f4c30ac771d60a584217b0a92f3761b
Luciano Eduardo <luciano123ab@gmail.com>
2025-07-05 14:56:07 -0300

    012(Introducao_ValidacaoDados)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,9 +11,14 @@ class AuthController extends Controller
     }
 
     public function loginSubmit(Request $request) {
-        echo $request->input("text_username");
-        echo "<br>";
-        echo $request->input("text_password");
+        //Form Validation:
+        $request->validate(["text_username" => "required", "text_password" => "required"]);
+
+        //Get User Input:
+        $username = $request->input("text_username");
+        $password = $request->input("text_password");
+        
+        echo "OK!";
     }
 
     public function logout() {
