dbe9fcacbd22ddc07a2ea9ad72ca03355bbcad4a
Luciano Eduardo <luciano123ab@gmail.com>
2025-07-05 15:54:52 -0300

    014(TextosPersonalizados_Erros)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,7 +12,15 @@ class AuthController extends Controller
 
     public function loginSubmit(Request $request) {
         //Form Validation:
-        $request->validate(["text_username" => "required", "text_password" => "required"]);
+        $request->validate(
+            //Rules: 
+            ["text_username" => "required|email", "text_password" => "required|min:6|max:16"],
+            //Error Messages:
+            [
+                "text_username.required" => "O username é obrigatório!", "text_username.email" => "Username deve ser um email válido!",
+                "text_password.required" => "A senha é obrigatória!", "text_password.min" => "A senha deve ter pelo menos :min caracteres!", "text_password.max" => "A senha deve ter no máximo :max caracteres!"
+            ]
+        );
 
         //Get User Input:
         $username = $request->input("text_username");
