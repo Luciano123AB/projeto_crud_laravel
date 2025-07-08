c134863633ab74b80c6e4eddc308cf601f48ecca
Luciano Eduardo <luciano@gmail.com>
2025-07-08 19:26:03 -0300

    004(Receber_Parametros-Routes)

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function index($value){
+    public function index($value) {
         return view("main", ["value" => $value]);
     }
 }
