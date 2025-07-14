12bc77e1cab92482a9560252049072deb6c740cb
Luciano Eduardo <luciano123ab@gmail.com>
2025-07-14 19:49:56 -0300

    021(Introducao_Middleware)

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
+use App\Http\Middleware\CheckIsLogged;
 use Illuminate\Support\Facades\Route;
 
 //Auth Routes:
@@ -9,4 +10,10 @@ Route::get("/login", [AuthController::class, "login"]);
 
 Route::post("/loginSubmit", [AuthController::class, "loginSubmit"]);
 
-Route::get("/logout", [AuthController::class, "logout"]);
+Route::middleware([CheckIsLogged::class])->group(function() {
+    Route::get("/", [MainController::class, "index"]);
+
+    Route::get("/newNote", [MainController::class, "newNote"]);
+
+    Route::get("/logout", [AuthController::class, "logout"]);
+});
