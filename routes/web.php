fe3cc4a1b41b8e706314c9ba2fdec76db2a72d63
Luciano Eduardo <luciano123ab@gmail.com>
2025-07-14 20:12:23 -0300

    022(Middleware_Adicional)

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,17 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use App\Http\Middleware\CheckIsLogged;
+use App\Http\Middleware\CheckIsNotLogged;
 use Illuminate\Support\Facades\Route;
 
-//Auth Routes:
-Route::get("/login", [AuthController::class, "login"]);
+//Auth Routes - User not Logged:
+Route::middleware([CheckIsNotLogged::class])->group(function() {
+    Route::get("/login", [AuthController::class, "login"]);
 
-Route::post("/loginSubmit", [AuthController::class, "loginSubmit"]);
+    Route::post("/loginSubmit", [AuthController::class, "loginSubmit"]);
+});
 
+//Auth Routes - User Logged:
 Route::middleware([CheckIsLogged::class])->group(function() {
     Route::get("/", [MainController::class, "index"]);
 
