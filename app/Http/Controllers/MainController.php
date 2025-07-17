6d5d59e500405fe8248f8336be140c6139c42a08
Luciano Eduardo <luciano123ab@gmail.com>
2025-07-16 22:57:16 -0300

    027(ApresentacaoNotas_UserLogado)

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -11,17 +11,10 @@ class MainController extends Controller
         
         //Load User's Notes:
         $id = session("user.id");
-        $user = User::find($id)->toArray();
         $notes = User::find($id)->notes()->get()->toArray();
 
-        echo "<pre>";
-            print_r($user);
-            print_r($notes);
-
-        die();
-
         //Show Home View:
-        return view("home");
+        return view("home", ["notes" => $notes]);
     }
 
     public function newNote() {
