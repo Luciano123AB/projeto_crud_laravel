33bd4af0bc01e03f82fb92900be3c62e50f845d0
Luciano Eduardo <luciano123ab@gmail.com>
2025-08-03 22:58:34 -0300

    035(ValidacaoFormulario_EditarNota)

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -61,6 +61,38 @@ class MainController extends Controller
         return view("edit_note", ["note" => $note]);
     }
 
+    public function editNoteSubmit(Request $request) {
+        //Validate Request:
+        $request->validate(
+            //Rules: 
+            ["text_title" => "required|min:3|max:200", "text_note" => "required|min:3|max:3000"],
+            //Error Messages:
+            [
+                "text_title.required" => "O título é obrigatório!", "text_title.min" => "O título deve ter pelo menos :min caracteres!", "text_title.max" => "O título deve ter no máximo :max caracteres!",
+                "text_note.required" => "A nota é obrigatória!", "text_note.min" => "A nota deve ter pelo menos :min caracteres!", "text_note.max" => "A nota deve ter no máximo :max caracteres!"
+            ]
+        );
+
+        //Check if note_id Exists:
+        if ($request->note_id == null) {
+            return redirect()->route("home");
+        }
+
+        //Decrypt note_id:
+        $id = Operations::decryptId($request->note_id);
+
+        //Load Note:
+        $note = Note::find($id);
+
+        //Update Note:
+        $note->title = $request->text_title;
+        $note->text = $request->text_note;
+        $note->save();
+
+        //Redirect to Home:
+        return redirect()->route("home");
+    }
+
     public function deleteNote($id) {
         
         $id = Operations::decryptId($id);
